31ec877195d2efccf9a5f735f35369faab04bccd
しょ <r02.gm.2100219@da.iwasaki.ac.jp>
2021-07-11 17:57:38 +0900

    mobileを編集

--- mobile-nav.js
+++ mobile-nav.js
@@ -1,12 +1,8 @@
-let f = false;
-
 $(document).ready(function()
 {
   $('.mobile-nav').click(function()
   {
-    $('.mobile-nav-menu').css({'cssText': `height: ${f ? '0' : '120px'} !important;`});
-    $('.moble-nav-inner a').css({'cssText': `font-size: ${f ? '0' : '24px'} !important;`});
-    $('.moble-nav-inner p:first-child').css({'cssText': `border-bottom: ${f ? 'none' : 'darkorange solid 3px'} !important;`});
-    f = !f;
+    $('.mobile-nav-icon-close').toggleClass('mobile-nav-icon-open');
+    $('.mobile-nav-menu-close').toggleClass('mobile-nav-menu-open');
   });
 });
