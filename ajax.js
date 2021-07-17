556b158becbd99b2834d19754cbbbfee25fd7888
しょ <r02.gm.2100219@da.iwasaki.ac.jp>
2021-07-17 23:49:56 +0900

    Ajax実装

---
+++ ajax.js
@@ -0,0 +1,57 @@
+/**
+ * ナビゲーション
+ * @param {string} rootPath 
+ * @param {Number} _Option_ 
+ */
+function nav(rootPath, _Option_)
+{
+  ajax_base('.nav', rootPath, 'nav.html', _Option_);
+}
+
+/**
+ * Ajaxベース
+ * @param {string} element Target Element Id or Class
+ * @param {string} rootPath Root Dir Path
+ * @param {string} filename Filename to Load
+ * @param {*} _Option_ Free Option
+ */
+function ajax_base(element, rootPath, filename, _Option_)
+{
+  $.ajax(
+  {
+    url     : `${rootPath}./include/${filename}`,
+    cache   : false,
+    success : function(html)
+    {
+      // --------------------------------------------
+      // ナビゲーション
+      const currentTabClass          = 'current';
+      const otherTabClass            = 'tab';
+      const targetCurrentCheckString = '$isCurrent';
+
+      html = html.replaceAll('{$root}', rootPath);
+      html = html.replaceAll(`href=""`, '');
+
+      for (let i = 0; i < 10; i++)
+      {
+        if (i == _Option_)
+        {
+          // @param {currentTabClass} に書き換え
+          html = html.replaceAll(`{${targetCurrentCheckString}${i}}`, currentTabClass);
+
+          // ナビゲーション規則 : <a class="" href=""></a>
+          html = html.replaceAll('class="current" href=', 'class="current"');
+        }
+        else
+        {
+          // @param {otherTabClass} に書き換え
+          html = html.replaceAll(`{${targetCurrentCheckString}${i}}`, otherTabClass); 
+        }
+      }
+      // --------------------------------------------
+
+      // 書き込み
+      $(element).html(html);
+    }
+  });
+}
