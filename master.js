71c3af39b227c78a0f1e1939d7e7363b54e418ba
しょ <r02.gm.2100219@da.iwasaki.ac.jp>
2021-08-10 20:33:19 +0900

    インデント調整

--- master.js
+++ master.js
@@ -1,15 +1,15 @@
 $(window).scroll(function()
-  {
-    let scrollY = $(this).scrollTop();
+{
+  let scrollY = $(this).scrollTop();
 
-    $('.moveToTop')
-      .css('transform', scrollY >= 200 ? 'scale(1)' : 'scale(0)')
-        .click(function()
-        {
-          window.scroll({ top: 0, behavior: 'smooth' });
-        })
-        .hover(function()
-        {
-          $(this).css('cursor', 'pointer');
-        })
+  $('.moveToTop')
+    .css('transform', scrollY >= 200 ? 'scale(1)' : 'scale(0)')
+      .click(function()
+      {
+        window.scroll({ top: 0, behavior: 'smooth' });
+      })
+      .hover(function()
+      {
+        $(this).css('cursor', 'pointer');
+      })
 });
